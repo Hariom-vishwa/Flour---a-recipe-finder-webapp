e66448910f0424bb5ca25b0a0d6cf3fea326b005
Hariom-vishwa <hawkgamerhv@gmail.com>
2025-03-26 00:01:21 +0530

    RecipeInfo Comp

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,38 +1,41 @@
-import React, { useState } from 'react'
-import './Homepage.css'
-import Cards from './Cards'
-
-
+import React, { useState } from "react";
+import "./Homepage.css";
+import Cards from "./Cards";
 
 export default function Homepage() {
+  const [mealsData, setMealsData] = useState();
+  const [searchVal, setSearchVal] = useState();
 
-  const [mealsData, setMealsData] = useState()
-  const [searchVal, setSearchVal] = useState()
+  const handleInput = (e) => {
+    setSearchVal(e.target.value);
+  };
 
-const handleInput = (e) => {
-setSearchVal(e.target.value)
-}
-
-  // fetching API 
+  // fetching API
 
   const fetching = async () => {
-    const gettingData = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchVal}`)
-    const getJsonData = await gettingData.json()
-    setMealsData(getJsonData.meals)
-  }
+    const gettingData = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchVal}`
+    );
+    const getJsonData = await gettingData.json();
+    setMealsData(getJsonData.meals);
+  };
 
   return (
     <>
-    <div className="navBar">
+      <div className="navBar">
         <div className="logo">
-            <h1>FLOUR</h1>
+          <h1>FLOUR</h1>
         </div>
         <div className="searchCont">
-            <input type="text" placeholder='Enter your recipe name' onChange={handleInput}/>
-            <button onClick={fetching}>Search</button>
+          <input
+            type="text"
+            placeholder="Enter your recipe name"
+            onChange={handleInput}
+          />
+          <button onClick={fetching}>Search</button>
         </div>
-    </div>
-    <Cards details={mealsData} />
+      </div>
+      <Cards details={mealsData} />
     </>
-  )
+  );
 }
--- src/Recipeinfo.jsx
+++ src/Recipeinfo.jsx
@@ -1,31 +1,56 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+// import "./Homepage.css";
 
 const Recipeinfo = () => {
-  const {mealid} = useParams()
-  const [mealRecipe, setMealRecipe] = useState()
+  const { mealid } = useParams();
+  const [mealRecipe, setMealRecipe] = useState();
   console.log(mealid);
 
-  const getMealById = async () =>{
-    const getMeal = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`)
-    const mealJson = await getMeal.json()
-    console.log(mealJson.meals[0])
-    setMealRecipe(mealJson.meals[0])
+  const getMealById = async () => {
+    const getMeal = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`
+    );
+    const mealJson = await getMeal.json();
+    console.log(mealJson.meals[0]);
+    setMealRecipe(mealJson.meals[0]);
+  };
+
+  if (mealid != "") {
+    getMealById();
   }
 
-  if(mealid != ""){
-    getMealById()
-  }
-  
+
+  // Logo styling 
+
+  const logoStyle = {
+    color: "var(--pri-col)",
+  } 
+
   return (
-    <div>
-      <img src="" alt="" />
-      <div>
-        <h1>{mealRecipe.strMeal} - Recipe Details</h1>
-        <button></button>
-      </div>
-    </div>
-  )
-}
+    <>
+      {!mealRecipe ? (
+        "Data Not Found"
+      ) : (
+        <>
+          <div className="logo">
+            <h1 style={logoStyle}>Flour</h1>
+          </div>
+          <div className="recipeCont">
+            <img src={mealRecipe.strMealThumb} alt="" />
+            <div className="recipeDets">
+              <div className="headingsButton">
+                <h1>Recipe Details</h1>
+                <button className="recipeBtn">{mealRecipe.strMeal}</button>
+                <h3>Instructions</h3>
+              </div>
+              <p>{mealRecipe.strInstructions}</p>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
-export default Recipeinfo
+export default Recipeinfo;
